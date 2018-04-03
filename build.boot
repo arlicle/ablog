f07c33736fabfd79047067f8814a02406617fca6
edison <arlicle@gmail.com>
2018-04-04 00:24:59 +0800

    删除无用代码

--- build.boot
+++ build.boot
@@ -33,6 +33,3 @@
    ))
 
 
-(deftask to-html []
-  (parse-file)
-  )
--- src/ablog/build.clj
+++ src/ablog/build.clj
@@ -29,7 +29,6 @@
   []
   (merge default-settings (read-string (slurp "settings.ini"))))
 
-(def posts-list (file-seq (clojure.java.io/file "posts")))
 
 (defn get-file-ext 
   "获取文件后缀名"
@@ -67,17 +66,6 @@
 
 
 
-; 获取皮肤
-(defn get-theme 
-  "获取当前的皮肤"
-  [settings]
-  (:theme settings "default"))
-
-; 获取对应皮肤下的模板路径
-(defn get-theme-filepath
-  "获取皮肤文件地址"
-  [settings template-filename]
-  (str "theme/" (get-theme settings) "/" template-filename))
 
 (defn get-public-post-filepath
   "获取源文件文件对应的public文件"
@@ -111,7 +99,7 @@
       )
 ))
 
-(defn get-posts-list
+(defn get-
   "获取所有的post文件列表，返回的是：
   文章内容
   文章标题
@@ -128,27 +116,8 @@
 
 
 
-(defn only-md-files
-  [file-s]
-  ;(println file-s)
-  (filter #(and (.isFile %) (.endsWith (.toString %) "md")) file-s))
-
-(defn get_md
-  [f]
-  (let [f-path (.toString f)]
-    (println f-path)
-    [f-path (clojure.string/replace f-path, #"\.md", ".html")]
-  )
-)
 
 
-(defn get-file-interpretor
-  "获取文件解释器，如果是不合法的文件，返回nil"
-  [file]
-)
-
-;获取上一篇文章
-;获取下一篇文章
 (defn generate-post
   "只为一篇当前修改的文章生成静态页面"
   [settings [prev-post post next-post]]
@@ -166,43 +135,8 @@
   "整站生成静态网站"
   []
   (let [settings (get-settings)
-        posts-list (partition 3 1 (lazy-cat [nil] (get-posts-list settings) [nil]))
+         (partition 3 1 (lazy-cat [nil] (get- settings) [nil]))
     ]
-    ; (doall
-      (pmap #(generate-post settings %) posts-list)
-    ; )
+      (pmap #(generate-post settings %) )
     ))
-
-
-
-; (deftask hello []
-;   (println "Hello world!******!"))
-; 用模板渲染内容
-
-(defn parse-file
-  "把所有的markdown文档找到然后编译为html"
-  []
-  (doseq [f (only-md-files posts-list)]
-    (try (let [rdr (clojure.java.io/reader f)
-        post-config (read (java.io.PushbackReader. rdr))
-        post-content (line-seq (java.io.BufferedReader. rdr))
-        [_ post-time-int post-filename] (re-find #"^(\d+)-(.*?)\.md$" (.getName f))
-        post-filepath (str "public/" post-filename ".html")
-        ;f-html-path (str "public/" (clojure.string/replace (.getName f) #"^(\d+)-(.*?)\.md$" "$2.html"))
-        post-html (render-file "theme/post.html" {:post-title (:title post-config) 
-            :post-content (md/md-to-html-string (clojure.string/join "\n" post-content))
-            :post-time (clj-time-format/parse (clj-time-format/formatter "yyyyMMdd") "20101211")})
-        ]
-
-        (println post-config)
-        (println post-filepath)
-        ; (println post-time)
-        (println post-filename)
-        (println _)
-        (println (clj-time-format/parse (clj-time-format/formatter "yyyyMMdd") "20101211"))
-        (spit post-filepath post-html)
-        )
-    (catch Exception e (do (println (.toString f) (println e)))))
-    (println f)))
-
 
